94a55d4519e86ed16cda600a381786e16ed9c247
dpadm <dpadm@MSI>
2020-10-28 23:06:32 +0530

    String Calculator changes

---
+++ StringCalculator/src/TestCalculator.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCalculator {
+
+	private Calculator calculator;
+	@Before
+	public void init()
+	{
+		calculator= new Calculator();
+	}
+	
+	@Test
+	public void emptyStringResturnsZero()
+	{
+		
+		assertEquals(calculator.calculate(""),0 );
+	}
+	@Test
+	public void singleValueIsReplied()
+	{
+		assertEquals(calculator.calculate("1"),1 );
+	}
+	@Test
+	public void twoNumbersDelimitedReturnSum()
+	{
+		assertEquals(calculator.calculate("1,2"),3 );
+	}
+}
